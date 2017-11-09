625721f7910cbaacb3e329f368fe65ef38aaa31e
Valeriyk <Valeriyk@TLV-DL-002.MaxiFX.net>
2017-11-09 14:49:43 +0200

    project structure change

--- src/main/java/kogan/herokuresttest/controller/RESTController.java
+++ src/main/java/kogan/herokuresttest/controller/RESTController.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-@EnableAutoConfiguration//(exclude={DataSourceAutoConfiguration.class})
+@ImportResource("classpath:beans.xml")
 public class RESTController {
 	
 	@RequestMapping("/")
