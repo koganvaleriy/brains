b76d2629b8a20309ebde0b95d1a5257f9b6a94bc
Valeriyk <Valeriyk@TLV-DL-002.MaxiFX.net>
2017-11-09 16:10:49 +0200

    project structure change

--- src/main/java/kogan/herokuresttest/controller/RESTController.java
+++ src/main/java/kogan/herokuresttest/controller/RESTController.java
@@ -1,9 +1,7 @@
 package kogan.herokuresttest.controller;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
